fa6beea153a78a5ece2e580e29b97c7b6d418be4
HammerPanzer92 <romdur59@gmail.com>
2024-07-08 22:37:51 +0200

    Feat : SCSS complété pour la page logement Todo : séparé le SCSS en fichier (1 fichier = 1 component) Todo : page à propos Todo : SCSS mobile

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function Collapse(props) {
+  const myRef = useRef(null);
+
+  const collapseClick = () => {
+    if (myRef) {
+      const btn = myRef.current;
+      btn.classList.toggle("active");
+      var content = btn.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    }
+  };
+
   if (props.map) {
     const contentMap = () =>
       props.content.map((element, index) => {
@@ -8,25 +23,25 @@ function Collapse(props) {
       });
 
     return (
-      <>
-        <button type="button" className="collapsible">
+      <div className="collapsible">
+        <button ref={myRef} onClick={collapseClick} type="button">
           {props.title}
         </button>
         <div className="content">
           <ul>{contentMap()}</ul>
         </div>
-      </>
+      </div>
     );
   } else {
     return (
-      <>
-        <button type="button" className="collapsible">
+      <div className="collapsible">
+        <button ref={myRef} onClick={collapseClick} type="button">
           {props.title}
         </button>
         <div className="content">
           <p>{props.content}</p>
         </div>
-      </>
+      </div>
     );
   }
 }
